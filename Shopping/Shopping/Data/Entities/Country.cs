7d769f76ba75b8a14fd3a6f2a73ad6a2b1443600
giovany77 <giovany77@hotmail.com>
2023-02-17 00:05:42 -0500

    CRUD Countries

---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name="Pais")]
+        [MaxLength(50, ErrorMessage ="El campo {0} debe tener maximo {1} caracteres.")] //tamaño maximo de caracteres, evitamos que se cree con max
+        [Required(ErrorMessage ="El campo {0} es obligatorio.")] //para que no sea nulo
+        public string Name { get; set; }
+    }
+}
--- Shopping/Shopping/Program.cs
+++ Shopping/Shopping/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Shopping.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DataContext>(o => 
+{
+    o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
